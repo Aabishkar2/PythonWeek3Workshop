bf394aba193c3b7b5c9de990e9a69cad7039df09
DESKTOP-99S35E1\Aabishkar <aabishkar2@gmail.com>
2018-09-04 21:31:26 +0545

    updated task.py

--- task.py
+++ task.py
@@ -8,19 +8,20 @@ class Check:
 
     def __init__(self):
         self.text = ""
-    
+
     def inputString(self):
         self.text = input("Enter the String:")
 
-    def checkString(self):
-        a = self.text
+      def checkString(self):
         e = []
+        a = self.text
+        l = len(a)
+        # print(l)
         b = list(a)
-        for x in b:
-            if x.isdigit():
-                pass
-            else:
-                e.append(x)
+        # print(b)
+        for x in range(0,l,2):
+            e.append(b[x])
+
 
         z = "".join(str(x) for x in e)
         print(z)
@@ -29,3 +30,6 @@ p =  Check()
 p.inputString()
 p.checkString()
 
+p =  Check()
+p.inputString()
+p.checkString()
